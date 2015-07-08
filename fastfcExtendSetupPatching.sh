3779eaf5f94429c7036a5485b254987a6a7f17c7
fastSimulations <fastSimulations@gmail.com>
2015-07-08 13:36:35 -0700

    Upload of install and patching process

---
+++ fastfcExtendSetupPatching.sh
@@ -0,0 +1,99 @@
+#! /bin/bash
+set +e
+clear
+echo
+echo "Fast-FC Setup Script for Foam-Extend-3.1"
+echo
+
+echo
+echo "Download Foam Extend 3.1"
+echo
+
+mkdir $HOME/foam >/dev/null 3>&1
+cd $HOME/foam
+tar xvzf $HOME/fastExtendSetup/EXTEND-3.1/foam-extend-3.1.tar.gz -C $HOME/foam/.
+
+#git clone http://git.code.sf.net/p/openfoam-extend/foam-extend-3.1 foam-extend-3.1
+
+echo 
+echo "Appending scripts to bashrc"
+echo
+
+cat $HOME/fastExtendSetup/fastfcBASHRC/bashrcAdditions >> $HOME/.bashrc
+cp $HOME/fastExtendSetup/fastfcBASHRC/extendBASHRC $HOME/.extendBASHRC >/dev/null 3>&1
+
+source $HOME/.bashrc
+
+echo 
+echo "Creating GCC 48 Compiler Options"
+echo
+
+cp -r $HOME/fastExtendSetup/extendGCC48/linux64Gcc48 $WM_PROJECT_DIR/wmake/rules/. >/dev/null 3>&1
+cd $WM_PROJECT_DIR
+git branch hotfix/gcc48
+git checkout hotfix/gcc48
+
+
+echo
+echo "Creating prefs.sh"
+echo
+cp -r $HOME/fastExtendSetup/extendPrefs/prefs.sh $WM_PROJECT_DIR/etc/. >/dev/null 3>&1
+
+echo
+echo "Compiling the FOAM_SRC"
+echo
+
+cd $FOAM_SRC
+wmakeLnIncludeAll 
+
+echo
+echo "Fixing Make File Scripts in ThirdParty Directory"
+echo
+
+cp $HOME/fastExtendSetup/fixedMakeScripts/AllMake.stage* $WM_THIRD_PARTY_DIR/. >/dev/null 3>&1
+
+echo
+echo "Copying Third Party Archives to rpmBuild"
+echo
+
+cp -r $HOME/fastExtendSetup/packageSources/SOURCES $WM_THIRD_PARTY_DIR/rpmBuild/ >/dev/null 3>&1
+cp -r $HOME/fastExtendSetup/packageSources/TGZS $WM_THIRD_PARTY_DIR/rpmBuild/. > /dev/null 3>&1
+
+echo 
+echo "Compiling Third Party Packages"
+echo
+
+cd $WM_THIRD_PARTY_DIR
+
+./AllMake.stage1 | tee stage1.compile.log
+echo 
+echo "Compiled Stage 1"
+echo
+
+./AllMake.stage2 | tee stage2.compile.log
+echo 
+echo "Compiled Stage 2"
+echo
+
+source $HOME/.bashrc
+
+./AllMake.stage3 | tee stage3.compile.log
+echo 
+echo "Compiled Stage 3"
+echo
+
+./AllMake.stage4 | tee stage4.compile.log
+echo 
+echo "Compiled Stage 4"
+echo
+
+
+grep -r $WM_THIRD_PARTY_DIR/*.compile.log "error"
+
+source $HOME/.bashrc
+
+cd $WM_PROJECT_DIR
+./Allwmake.firstInstall | tee extend.compile.log
+
+echo
+echo "Script Ended"
---
+++ fastfcOpenSuseSetup.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+echo
+echo "Fast-FC Setup Script for Foam-Extend-3.1"
+echo
+
+# patterns needed
+
+neededPattern=(
+	devel_qt4
+	devel_C_C++
+)
+neededPackages=(
+	gcc 
+	gcc-c++
+	gcc-fortran
+	libstdc++-devel 
+	mercurial 
+	git 
+	flex 
+	bison 
+	make 
+	ccache 
+	zlib-devel 
+	rpm-build 
+	binutils-devel 
+	cmake 
+	openmpi-devel 
+	gnuplot 
+	m4 
+	libqt4
+	libqt4-devel
+	libQtWebKit-devel 
+	python
+)
+bonusPackages=(
+	emacs 
+	csh 
+	tcsh 
+	zsh 
+	gvim
+	gedit
+)
+
+for p in ${neededPattern[@]}; do
+    sudo zypper -n install -t pattern $p
+done
+
+for p in ${neededPackages[@]}; do
+    sudo zypper -n install $p
+done
+
+for p in ${bonusPackages[@]}; do
+    sudo zypper -n install $p
+done
+
+echo
+echo "Patching glxext.h in /usr/include/GL"
+sudo cp $HOME/fastExtendSetup/GLFix/glxext.h /usr/include/GL/.
+echo "		--- Done"
+
+echo
+echo "Script Ended"
